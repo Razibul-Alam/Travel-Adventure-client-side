3ef24a7640b74b8b5bb73b55d15ce6336bfe2523
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-10-31 07:26:12 +0600

    readme file edit

--- src/Pages/All-adventures/AllAdventures.js
+++ src/Pages/All-adventures/AllAdventures.js
@@ -1,20 +1,26 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import SingleAdventure from './SingleAdventure';
-import { Row } from 'react-bootstrap';
+import { Row,Spinner } from 'react-bootstrap';
 const AllAdventures = () => {
+    const [initialLoading,setInitialLoading]=useState(false)
     const[adventures,setAdventures]=useState([])
     useEffect(()=>{
+        setInitialLoading(true)
         axios.get(`https://hidden-bayou-72012.herokuapp.com/allEvents`)
         .then(res => {
           const allAdventure = res.data;
           console.log(allAdventure)
           setAdventures(allAdventure)
+          setInitialLoading(false)
           
         })
     },[])
     return (
         <>
+        {initialLoading&&<div className='d-flex justify-content-center mt-5'><Spinner animation="border" variant="danger" role="status">
+  <span className="visually-hidden">Loading...</span>
+</Spinner></div>}
         <div className='my-5'>
         <h2 className='text-center text-success'>Our Packages</h2>
         <hr className='m-auto text-danger bolder w-25'/>
